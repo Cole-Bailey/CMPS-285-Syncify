e3453f748b1ff16937f33e9198fed4cfb294db9f
QuackyDaddy <zachary.duncan@selu.edu>
2022-11-03 23:39:13 -0500

    Calendar component made
    To get this component to work, look at the front-end thread on Discord and run the commands I posted in your terminal

---
+++ Web/LearningStarter/learning-starter-web/src/pages/calendar-page/calendar-page.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import App from "../../components/calendar/calendar"
+
+function CalendarComponent(){
+    return (
+        <div className = "calendarContainer">
+            <App/>
+        </div>
+    )
+}
+export default CalendarComponent
